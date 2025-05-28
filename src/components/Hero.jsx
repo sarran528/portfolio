a25a455d469b767bedb44684e1f5b88d8cb1dff7
sarran528 <sarranm528@gmail.com>
2025-05-28 21:05:28 +0530

    links updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -78,41 +78,41 @@ const Hero = () => {
       </div>
 
       <div className="absolute right-5 top-1/2 -translate-y-1/2 flex flex-col gap-5 items-center md:mt-10 mt-5 md:gap-12">
-        <Link target="_blank" to="https://www.linkedin.com/in/jeffjiang13/">
+        <Link target="_blank" to="https://www.linkedin.com/in/sarran-m-52a96a331?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B8raV4zkNQ1SlWxhqmbdToQ%3D%3D">
           <BsLinkedin
             size={30}
             className="icon hover:text-[#0e76a8] hover:translate-y-[-2px] hover:cursor-pointer transition-all ease-in duration-100"
           />
         </Link>
 
-        <Link target="_blank" to="https://github.com/jeffjiang13">
+        <Link target="_blank" to="https://github.com/sarran528/sarran528">
           <BsGithub
             size={30}
             className="icon hover:text-[#494646] hover:translate-y-[-2px] hover:cursor-pointer transition-all ease-in duration-100"
           />
         </Link>
 
-        <Link target="_blank" to="https://twitter.com/jeffjiang9">
+        <Link target="_blank" to="">
           <BsTwitter
             size={30}
             className="icon hover:text-[#1DA1F2] hover:translate-y-[-2px] hover:cursor-pointer transition-all ease-in duration-100"
           />
         </Link>
 
-        <Link target="_blank" to="https://discord.com/channels/@jeff.jiang13">
+        <Link target="_blank" to="">
           <BsDiscord
             size={30}
             className="icon hover:text-[#5865F2] hover:translate-y-[-2px] hover:cursor-pointer transition-all ease-in duration-100"
           />
         </Link>
 
-        <Link target="_blank" to="https://www.instagram.com/trainer.jeff212/">
+        <Link target="_blank" to="https://www.instagram.com/_.sarran05._/">
           <BsInstagram
             size={30}
             className="icon hover:text-[#E1306C] hover:translate-y-[-2px] hover:cursor-pointer transition-all ease-in duration-100"
           />
         </Link>
-        <Link target="_blank" to="https://www.facebook.com/jeff.jiang.9/">
+        <Link target="_blank" to="">
           <BsFacebook
             size={30}
             className="icon hover:text-[#1877F2] hover:translate-y-[-2px] hover:cursor-pointer transition-all ease-in duration-100"
